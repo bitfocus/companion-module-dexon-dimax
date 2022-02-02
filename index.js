bc757735b13964317b9046e421a82241a0c5890a
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:22 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ instance.prototype.init_presets = function () {
 instance.prototype.actions = function(system) {
 	var self = this;
 
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'login': {
 			label: 'Force Login',
